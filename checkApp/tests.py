2ecc71b86ee564a886bb1520e559818139aeac35
dan <dsb@tentacle.co>
2012-02-16 18:04:26 -0500

    setup the initial testCase and CheckTestHelper to handle creating models in the db

--- checkApp/tests.py
+++ checkApp/tests.py
@@ -4,13 +4,49 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from checkApp.models import CheckList, CheckItem
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class CheckTestHelper(object):
+    def create_checklist(self, *args, **kwargs):
+        id = CheckList.objects.all().count()
+        defaults = {
+            'name':'test',
+            'creator':'tester'
+        }
+        defaults.update(kwargs)
+        return CheckList.objects.create(**defaults)
+
+    def create_checkItem(self, *args, **kwargs):
+        id = CheckItem.objects.all().count()
+        if kwargs.get('pk'):
+            pk = kwargs.get('pk')
+        else:
+            pk = 1
+        defaults = {
+            'text' : 'test this',
+            'done' : False,
+            'checkList' : CheckList.objects.get(id=pk)
+        }
+        return CheckItem.objects.create(**defaults)
+
+
+class IndexPageTestCase(CheckTestHelper,TestCase):
+    def setUp(self):
+        CheckTestHelper.create_checklist(self)
+        CheckTestHelper.create_checkItem(self, text='test1')
+        CheckTestHelper.create_checkItem(self, text='test2')
+        self.url = reverse('index')
+
+
+    def test_pageLoads(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkApp/index.html')
+
+    def test_pageContext(self):
+        context = self.client.get(self.url).context
+        self.assertIn('list',context)
