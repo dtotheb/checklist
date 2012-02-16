667fd5940e1ba15d6cdf483872e174ada976a065
dan <dsb@tentacle.co>
2012-02-15 22:52:52 -0500

    pep8'd stuff

--- checkApp/migrations/0001_initial.py
+++ checkApp/migrations/0001_initial.py
@@ -4,10 +4,10 @@ from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Adding model 'CheckList'
         db.create_table('checkApp_checklist', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
@@ -16,12 +16,10 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal('checkApp', ['CheckList'])
 
-
     def backwards(self, orm):
-        
+
         # Deleting model 'CheckList'
         db.delete_table('checkApp_checklist')
-
 
     models = {
         'checkApp.checklist': {
