fbc377de8e723b53784b3625ba25243c1dfb4d52
dan <dsb@tentacle.co>
2012-02-15 22:02:41 -0500

    setup index view to grab all the checklists

--- checkApp/views.py
+++ checkApp/views.py
@@ -1,5 +1,10 @@
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
+from checkApp.models import CheckList,CheckItem
 
 def index(request):
-    return render(request, 'checkApp/index.html')
+    clist = CheckList.objects.all()
+    context = {
+        'list': clist,
+    }
+    return render(request, 'checkApp/index.html', context)
