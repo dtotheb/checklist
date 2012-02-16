a080166f553925d937bb6a604c716c275bb77ff8
dan <dsb@tentacle.co>
2012-02-15 20:44:55 -0500

    created initial checkItem model

---
+++ checkApp/migrations/0002_auto__add_checkitem.py
@@ -0,0 +1,43 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'CheckItem'
+        db.create_table('checkApp_checkitem', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('text', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('done', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('checkList', self.gf('django.db.models.fields.related.ForeignKey')(related_name='items', to=orm['checkApp.CheckList'])),
+        ))
+        db.send_create_signal('checkApp', ['CheckItem'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'CheckItem'
+        db.delete_table('checkApp_checkitem')
+
+
+    models = {
+        'checkApp.checkitem': {
+            'Meta': {'object_name': 'CheckItem'},
+            'checkList': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'items'", 'to': "orm['checkApp.CheckList']"}),
+            'done': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'checkApp.checklist': {
+            'Meta': {'object_name': 'CheckList'},
+            'creator': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['checkApp']
--- checkApp/models.py
+++ checkApp/models.py
@@ -7,3 +7,12 @@ class CheckList(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class CheckItem(models.Model):
+    text = models.CharField(max_length=255)
+    done = models.BooleanField(default=False)
+    checkList = models.ForeignKey(CheckList,related_name='items')
+
+    def __unicode__(self):
+        return self.text
